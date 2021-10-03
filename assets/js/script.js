c2cf6da78f9d1ce6834c120c0b2c45c27f2c01e1
saramorrin <sara.lee.morrin@googlemail.com>
2021-10-03 14:48:04 +0000

    Add code to generate random results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 /** Declare constants for DOM elements and possible choices */
 
 const message = document.getElementById("results-message");
-const playerScore = document.getElementById("player-score");
-const computerScore = document.getElementById("computer-score");
+let playerScore = document.getElementById("player-score");
+let computerScore = document.getElementById("computer-score");
 const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const rock = document.getElementById("rock");
@@ -11,35 +11,103 @@ const scissors = document.getElementById("scissors");
 const lizard = document.getElementById("lizard");
 const spock = document.getElementById("spock");
 
+/** Display results */
+
+
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+  const randomNumber = Math.floor(Math.random() * 5);
+  return choices[randomNumber];
+}
+
+function win(playerChoice, computerChoice) {
+  playerScore++;
+  playerScore.innerHTML = playerScore;
+  computerScore.innerHTML = computerScore;
+  message.innerHTML = playerChoice + "beats" + computerChoice + " you win! ";
+ 
+}
+
+function lose() {
+  console.log("lose");
+}
+
+function draw() {
+  console.log("draw");
+}
+
+/**
+   * The main game function, generate random choices
+   */
+
+function game(playerChoice) {
+  const computerChoice = getComputerChoice();
+  switch (playerChoice + computerChoice) {
+    case "rockscissors":
+    case "scissorspaper":
+    case "paperrock":
+    case "rocklizard":
+    case "lizardspock":
+    case "spockscissors":
+    case "scissorslizard":
+    case "lizardpaper":
+    case "paperspock":
+    case "spockrock":
+      win(playerChoice, computerChoice);
+      break;
+    case "scissorsrock":
+    case "paperscissors":
+    case "rockpaper":
+    case "lizardrock":
+    case "spocklizard":
+    case "scissorsspock":
+    case "lizardscissors":
+    case "paperlizard":
+    case "spockpaper":
+    case "rockspock":
+      lose(playerChoice, computerChoice);
+      break;
+    case "rockrock":
+    case "paperpaper":
+    case "scissorsscissors":
+    case "lizardlizard":
+    case "spockspock":
+      draw(playerChoice, computerChoice);
+      break;
+  }
+  
 /** 
  * Add event listener for each choice
  */
 
-
+}
+function main() {
   rock.addEventListener('click', function() {
-    console.log("hey");
+    game("rock");
   })
 
   paper.addEventListener('click', function() {
-    console.log("hey");  
+    game("paper");  
   })
 
   scissors.addEventListener('click', function() {
-    console.log("hey");
+    game("scissors");
   })
 
   lizard.addEventListener('click', function() {
-    console.log("hey");
+    game("lizard");
   })
+
 
   spock.addEventListener('click', function() {
-    console.log("hey");
+    game("spock");
   })
+}
+
+  main();
   
 
-  /**
-   * The main game function, generate random choices
-   */
+  
 
   
 
