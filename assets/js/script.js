4d0187ca7da9b24195ed24f5d693ef06574c2993
saramorrin <sara.lee.morrin@googlemail.com>
2021-10-06 10:51:06 +0000

    Add contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,9 +44,7 @@ function draw(playerChoice, computerChoice) {
   computerImage.src = `assets/images/${computerChoice}-result.png`
 }
 
-/**
-   * The main game function, generate random choices
-   */
+/** The main game function, generate random choices */
 
 function game(playerChoice) {
   const computerChoice = getComputerChoice();
@@ -84,9 +82,7 @@ function game(playerChoice) {
       break;
   }
   
-/** 
- * Add event listener for each choice
- */
+/** Add event listener for each choice */
 
 }
 function main() {
@@ -114,13 +110,7 @@ function main() {
 
   main();
   
-
-  
-
-  
-
-
-
+/** How To Play Modal */
 
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -130,6 +120,34 @@ var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+/** Get In Touch Modal */
+
+// Get the modal
+var modal = document.getElementById("contactModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("contactBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-contact")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
